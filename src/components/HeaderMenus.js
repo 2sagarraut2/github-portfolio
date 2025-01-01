744b257960d37ea9c12626e24d3b9f0983e71fd7
Sagar Raut <2sagarraut2@gmail.com>
2025-01-01 10:44:31 +0530

    Github UI cloned

---
+++ src/components/HeaderMenus.js
@@ -0,0 +1,14 @@
+const HeaderMenus = ({ MENU }) => {
+  const { label, element, id } = MENU;
+  //   console.log(label, element);
+  return (
+    <li key={id} className="border-b-2 border-red-600">
+      <button className="p-2 flex items-center border-b-1 border-red-600 text-sm">
+        {element}
+        <span className="font-light">{label}</span>
+      </button>
+    </li>
+  );
+};
+
+export default HeaderMenus;
